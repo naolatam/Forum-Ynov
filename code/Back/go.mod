f6822c0d84b084c72db4f7e49e8803bd89b642d2
Mloan <loan.mata@ynov.com>
2025-05-27 13:40:10 +0200

    Feat(env): load environment file at start-up in main.go

--- code/Back/go.mod
+++ code/Back/go.mod
@@ -1,3 +1,5 @@
 module Forum-back
 
 go 1.24.3
+
+require github.com/joho/godotenv v1.5.1
---
+++ code/Back/internal/config/env.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	// Load the .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+		os.Exit(1)
+	}
+
+}
