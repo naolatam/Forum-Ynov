00dd5eb4b69ce43994e974062a24e99eb3001f8d
Mloan <loan.mata@ynov.com>
2025-05-28 09:26:55 +0200

    Fix(model): adding session related attributes into User model

--- code/src/pkg/models/user.go
+++ code/src/pkg/models/user.go
@@ -6,13 +6,15 @@ import (
 
 // Category représente une ligne dans la table category
 type User struct {
-	ID        uuid.UUID
-	Pseudo    string
-	Email     string
-	Password  string
-	Bio       string
-	Avatar    string
-	CreatedAt string
-	role_id   uuid.UUID
-	Role      Role
+	ID         uuid.UUID
+	Pseudo     string
+	Email      string
+	Password   string
+	Bio        string
+	Avatar     string
+	CreatedAt  string
+	role_ID    uuid.UUID
+	Role       Role
+	session_ID string
+	Session    Session
 }
