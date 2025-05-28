b591dbd07e80aa078aaf2c027f516e750bc48b8e
Mloan <loan.mata@ynov.com>
2025-05-28 13:14:39 +0200

    Feat(main): update main function to start hosted services and remove unused os import

--- code/src/cmd/main.go
+++ code/src/cmd/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"Forum-back/internal/config"
-	"os"
+	hostedservices "Forum-back/pkg/hostedServices"
 )
 
 func main() {
 	config.LoadEnv()
-	println(os.Getenv("GOOGLE_CLIENT_SECRET"))
+	go hostedservices.StartAllHostedServices()
 }
