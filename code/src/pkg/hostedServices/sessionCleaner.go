42d3adcd52998b55f4e43bb3015fcaa841278239
Mloan <loan.mata@ynov.com>
2025-05-28 13:14:03 +0200

    Feat(hostedservices): add session cleaner hosted service for automatic session deletion

---
+++ code/src/pkg/hostedServices/sessionCleaner.go
@@ -0,0 +1,40 @@
+package hostedservices
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"Forum-back/internal/config"
+)
+
+func startSessionCleanerHostedService(ctx context.Context) {
+	log.Println("[HostedService] Session cleaner service started")
+	ticker := time.NewTicker(3 * time.Hour) // Service will run every 3 hours
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			log.Panicln("[HostedService] Running session cleaner service...")
+
+			db, err := config.OpenDBConnection()
+			if err != nil {
+				panic(err)
+			}
+
+			row, err := db.QueryContext(ctx, "DELETE FROM sessions WHERE expireAt < NOW()")
+			if err != nil {
+				log.Printf("[HostedService] Error cleaning sessions: %v", err)
+				continue
+			}
+			row.Close()
+			db.Close()
+		case <-ctx.Done():
+			log.Println("[HostedService] Session cleaner service stopped")
+			ticker.Stop()
+			return
+		}
+	}
+
+}
