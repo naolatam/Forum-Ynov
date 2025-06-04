a4f248d0839b4a0434dac330eba8faa3e5bbf19a
DantesDels <delver.sebastien@gmail.com>
2025-06-04 14:57:07 +0200

    feat(user): add FindById method to UserRepository for user retrieval by ID

--- code/src/pkg/repositories/userRepository.go
+++ code/src/pkg/repositories/userRepository.go
@@ -12,6 +12,28 @@ type UserRepository struct {
 	db *sql.DB
 }
 
+func (repository *UserRepository) FindById(id uuid.UUID) (*models.User, error) {
+	if repository.db == nil {
+		return nil, errors.New("connection to database isn't established")
+	}
+	rows, err :=
+		repository.db.Query("SELECT * FROM users WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var user models.User
+	if rows.Next() {
+		err = rows.Scan(&user.ID, &user.Pseudo, &user.Email, &user.Password, &user.Bio, &user.Avatar, &user.CreatedAt, &user.Role_ID, &user.Google_ID, &user.Github_ID)
+		if err != nil {
+			return nil, err
+		}
+		return &user, nil
+	}
+	return nil, errors.New("user not found")
+}
+
 func (repository *UserRepository) FindByIdOrUsernameOrEmail(id uuid.UUID, pseudo string, email string) (*models.User, error) {
 	if repository.db == nil {
 		return nil, errors.New("connection to database isn't established")
