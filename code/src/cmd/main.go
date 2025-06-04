df85e733df06c40c2b964b27c7c486cf7623e645
Mloan <loan.mata@ynov.com>
2025-06-04 13:33:14 +0200

    Fix(main): reset main to default and removed test from it

--- code/src/cmd/main.go
+++ code/src/cmd/main.go
@@ -4,33 +4,11 @@ import (
 	"Forum-back/internal/config"
 	"Forum-back/internal/server"
 	hostedservices "Forum-back/pkg/hostedServices"
-	"Forum-back/pkg/repositories"
-	"github.com/google/uuid"
-	"log"
 )
 
 func main() {
 	config.LoadEnv()
 	go hostedservices.StartAllHostedServices()
 
-	// Initialisation du repository
-	userRepo := &repositories.UserRepository{}
-	if !userRepo.Init() {
-		log.Fatal("Échec de l'initialisation du UserRepository")
-	}
-	defer userRepo.Close()
-
-	// Utilisation des valeurs existantes dans la base de données
-	id := uuid.MustParse("123e4567-e89b-12d3-a456-426614174000") // UUID de l'utilisateur inséré
-	pseudo := "testPseudo"
-	email := "test@example.com"
-
-	// Test de la fonction FindByIdOrUsernameOrEmail
-	user, err := userRepo.FindByIdOrUsernameOrEmail(id, pseudo, email)
-	if err != nil {
-		log.Printf("Erreur : %v\n", err)
-	} else {
-		log.Printf("Utilisateur trouvé : %+v\n", user)
-	}
 	server.StartServer()
 }
