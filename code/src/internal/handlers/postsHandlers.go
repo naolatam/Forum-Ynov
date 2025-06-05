5c3765fa0615a35f0143049a83b2cb27424445d8
DantesDels <delver.sebastien@gmail.com>
2025-06-05 17:26:25 +0200

    feat(profileHandlers): enhance profile handling with template parsing and error management

--- code/src/internal/handlers/postsHandlers.go
+++ code/src/internal/handlers/postsHandlers.go
@@ -1,19 +1,49 @@
 package handlers
 
-import "net/http"
+import (
+	"Forum-back/internal/config"
+	"net/http"
+	"log"
+)
 
 func SearchPostsHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "internal/templates/findPublication.gohtml")
+
+	db, err := config.OpenDBConnection()
+	if err != nil {
+		log.Println("Error connecting to the database:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
 }
 
 func SeePostHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "internal/templates/publication.gohtml")
+
+	db, err := config.OpenDBConnection()
+	if err != nil {
+		log.Println("Error connecting to the database:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
 }
 
 func NotForNowHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This feature is not implemented yet."))
+
+	// What to do here ?
 }
 
 func EditPostHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "internal/templates/publicationEdit.gohtml")
+
+	db, err := config.OpenDBConnection()
+	if err != nil {
+		log.Println("Error connecting to the database:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
 }
