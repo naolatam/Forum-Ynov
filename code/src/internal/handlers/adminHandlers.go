1eb095c9d225370907a1140000fca2160617b3d5
Torolgo <ylan.dessenne.spam@gmail.com>
2025-06-10 01:02:20 +0200

    feat(findPublication): solving the size difference between category's search bar and filter and search's button

--- code/src/internal/handlers/adminHandlers.go
+++ code/src/internal/handlers/adminHandlers.go
@@ -3,5 +3,5 @@ package handlers
 import "net/http"
 
 func AdminHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "internal/templates/admin.html")
+	http.ServeFile(w, r, "internal/templates/admin.gohtml")
 }
