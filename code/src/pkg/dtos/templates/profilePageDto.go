64515a4563c1c9d9b9aaa3921d8bc8140817f59d
Mloan <loan.mata@ynov.com>
2025-06-10 23:10:30 +0200

    feat(dtos): add ProfilePageDto and RecentActivityDto for user profile and activity representation

---
+++ code/src/pkg/dtos/templates/profilePageDto.go
@@ -0,0 +1,21 @@
+package dtos
+
+import "html/template"
+
+type ProfilPageDto struct {
+	Header         HeaderDto            `json:"header"`
+	Username       string               `json:"username"`
+	Email          string               `json:"email"`
+	JoinedAt       string               `json:"joined_at"`
+	Avatar         template.URL         `json:"avatar"`
+	Bio            string               `json:"bio"`
+	PostsCount     int                  `json:"posts_count"`
+	CommentsCount  int                  `json:"comments_count"`
+	RecentActivity []*RecentActivityDto `json:"recent_activity"`
+	Error          ProfilePageErrorDto  `json:"error_string,omitempty"`
+}
+
+type ProfilePageErrorDto struct {
+	ErrorType    string `json:"error_type"`
+	ErrorMessage string `json:"error_message"`
+}
