3b2ff1160591b3c0cdc7d2fd4aed0882ce0dc099
Mloan <loan.mata@ynov.com>
2025-06-10 23:14:51 +0200

    refactor(profile): improve modal handling and update user data bindings

--- code/src/internal/frontEnd/static/js/profile.js
+++ code/src/internal/frontEnd/static/js/profile.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const openModalButton = document.querySelector('#edit-profile');
     const modal = document.getElementById('profile-modal');
     const closeModalButton = document.getElementById('close-modal');
@@ -9,26 +9,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const cancelDeleteButton = document.getElementById('cancel-delete');
     const confirmDeleteButton = document.getElementById('confirm-delete');
 
-    openModalButton.addEventListener('click', function(e) {
+    openModalButton.addEventListener('click', function (e) {
         e.preventDefault();
         modal.classList.remove('hidden');
         document.body.classList.add('overflow-hidden');
     });
 
-    const closeModal = function() {
+    const closeModal = function () {
         modal.classList.add('hidden');
         document.body.classList.remove('overflow-hidden');
     };
 
-    deleteButton.addEventListener('click', function() {
+    deleteButton.addEventListener('click', function () {
         confirmDeleteModal.classList.remove('hidden');
     });
 
-    cancelDeleteButton.addEventListener('click', function() {
+    cancelDeleteButton.addEventListener('click', function () {
         confirmDeleteModal.classList.add('hidden');
     });
 
-    confirmDeleteButton.addEventListener('click', function() {
+    confirmDeleteButton.addEventListener('click', function () {
         // logique pour supprimer le compte
         confirmDeleteModal.classList.add('hidden');
         modal.classList.add('hidden');
@@ -39,9 +39,11 @@ document.addEventListener('DOMContentLoaded', function() {
     modalBackdrop.addEventListener('click', closeModal);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const editAvatarBtn = document.querySelector('.relative.mb-3 button');
     const avatarUploadModal = document.getElementById('avatar-upload-modal');
+    const avatarUploadErrorModal = document.getElementById('avatar-upload-error-modal');
+    const avatarUploadErrorCloseBtnModal = document.getElementById('avatar-close-error');
     const closeAvatarModalBtn = document.getElementById('close-avatar-modal');
     const cancelAvatarUploadBtn = document.getElementById('cancel-avatar-upload');
     const avatarInput = document.getElementById('avatar-upload');
@@ -59,6 +61,12 @@ document.addEventListener('DOMContentLoaded', function() {
         avatarUploadModal.classList.add('hidden');
     }
 
+    avatarUploadErrorCloseBtnModal.addEventListener('click', () => {
+        avatarUploadErrorModal.classList.add('hidden');
+        avatarInput.value = ''; // Clear the input
+    }
+    );
+
     if (closeAvatarModalBtn) closeAvatarModalBtn.addEventListener('click', closeAvatarModal);
     if (cancelAvatarUploadBtn) cancelAvatarUploadBtn.addEventListener('click', closeAvatarModal);
 
@@ -68,10 +76,16 @@ document.addEventListener('DOMContentLoaded', function() {
             const file = e.target.files[0];
             if (file) {
                 const reader = new FileReader();
-                reader.onload = function(e) {
-                    avatarPreview.src = e.target.result;
+                reader.onload = function (e) {
+                    if (file.size > 5 * 1024 * 1024) { // 2MB limit
+                        avatarUploadErrorModal.classList.remove('hidden');
+                        avatarInput.value = ''; // Clear the input
+                    } else {
+                        avatarPreview.src = e.target.result;
+                    }
                 }
                 reader.readAsDataURL(file);
+
             }
         });
     }
@@ -89,6 +103,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     initializePagination('content-tab', '.content-item', '.pagination-info-content', '.pagination-controls-content', 4);
 });
