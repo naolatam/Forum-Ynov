611ecc58f568a1822e8b2f8b51ddaf31d24cf962
Mloan <loan.mata@ynov.com>
2025-06-11 11:46:15 +0200

    Feat(profile): enhance user profile handling with custom error responses and refactor data creation

--- code/src/internal/handlers/profileHandlers.go
+++ code/src/internal/handlers/profileHandlers.go
@@ -23,20 +23,30 @@ func ProfileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer db.Close()
+	userService := services.NewUserService(db) // Init all services
+	postService := services.NewPostService(db)
+	commentService := services.NewCommentService(db)
+	sessionService := services.NewSessionService(db)
+	isConnected, session := sessionService.IsAuthenticated(r)
 
-	data := map[string]interface{}{
-		/* "id":     UserID,
-		"pseudo": Pseudo,
-		"email":  Email, */
-	}
-
-	tmpl, err := template.ParseFiles("internal/templates/profile.gohtml", "internal/templates/components/headerComponent.gohtml")
-	if err != nil {
-		ShowTemplateError500(w, &dtos.HeaderDto{})
+	// Fetch user_id from URL query
+	userUuid := getUserIdFromURL(w, r, isConnected, &session.User_ID)
+	if userUuid == uuid.Nil {
 		return
 	}
 
-	tmpl.Execute(w, data)
+	user := userService.FindById(userUuid) // Find the user with the given ID
+	if user == nil {                       // If no user is found, send a Not Found error page
+		ShowCustomError404(w, &dtos.HeaderDto{IsConnected: isConnected}, "User not found.")
+		return
+	}
+
+	user.Email = "private"                          // Set the email to private. Goal is to not share the email with anyone
+	postCount := postService.GetUserPostCount(user) // Get user stats
+	commentCount := commentService.GetUserCommentCount(user)
+	data := createDto(isConnected, user, nil, nil, false, postCount, commentCount) // Create a DTO from data received
+
+	showProfilePage(w, data)
 }
 
 func MyProfileHandler(w http.ResponseWriter, r *http.Request) {
@@ -49,6 +59,7 @@ func MyProfileHandler(w http.ResponseWriter, r *http.Request) {
 
 	sessionService := services.NewSessionService(db)
 	userService := services.NewUserService(db)
+
 	isConnected, session := sessionService.IsAuthenticated(r)
 	if !isConnected {
 		http.Redirect(w, r, "/auth/login", http.StatusSeeOther)
@@ -60,28 +71,8 @@ func MyProfileHandler(w http.ResponseWriter, r *http.Request) {
 		ShowCustomError500(w, &dtos.HeaderDto{IsConnected: isConnected}, "Unable to retrieve user information from the database.")
 		return
 	}
-
-	data := dtos.ProfilPageDto{
-		Header: dtos.HeaderDto{
-			IsConnected: isConnected,
-		},
-		Username:       user.Pseudo,
-		Email:          user.Email,
-		JoinedAt:       user.CreatedAt.Format("January 02, 2006"),
-		Avatar:         template.URL(utils.ConvertBytesToBase64(user.Avatar, "image/png")),
-		Bio:            user.Bio,
-		PostsCount:     0,
-		CommentsCount:  0,
-		RecentActivity: nil, // This should be populated with actual recent activity data
-	}
-
-	tmpl, err := templates.GetTemplateWithLayout(&data.Header, "myProfile", "internal/templates/profile.gohtml")
-	if err != nil {
-		ShowTemplateError500(w, &data.Header)
-		return
-	}
-
-	tmpl.Execute(w, data)
+	data := createDto(isConnected, user, nil, nil, true, 0, 0)
+	showProfilePage(w, data)
 }
 
 func DeleteMyProfileHandler(w http.ResponseWriter, r *http.Request) {
@@ -119,6 +110,7 @@ func DeleteMyProfileHandler(w http.ResponseWriter, r *http.Request) {
 	deleteSessionCookie(w)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
 func EditMyProfileHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		ShowError405(w, &dtos.HeaderDto{})
@@ -146,7 +138,8 @@ func EditMyProfileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if err := updateUserProfile(user, userService, r); err != nil {
-		ShowCustomError500(w, &dtos.HeaderDto{IsConnected: isConnected}, "Failed to update profile: "+err.Error())
+		data := createDto(isConnected, user, nil, err, true, 0, 0)
+		showProfilePage(w, data)
 		return
 	}
 
@@ -183,8 +176,74 @@ func updateUserProfile(user *models.User, userService *services.UserService, r *
 	}
 
 	if success := userService.Update(user); !success {
-		return errors.New("failed to update user profile")
+		return &dtos.ProfilePageErrorDto{
+			ErrorMessage: "Failed to update user profile in the database.",
+			ErrorTitle:   "Database Update Error",
+		}
 	}
 
 	return nil
+
+}
+
+func showProfilePage(w http.ResponseWriter, data *dtos.ProfilPageDto) error {
+	tmpl, templateError := templates.GetTemplateWithLayout(&data.Header, "myProfile", "internal/templates/profile.gohtml")
+	if templateError != nil {
+		ShowTemplateError500(w, &data.Header)
+		return templateError
+	}
+	if templateError = tmpl.Execute(w, data); templateError != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+	return nil
+}
+
+func createDto(isConnected bool, user *models.User, RecentActivity []*dtos.RecentActivityDto, err *dtos.ProfilePageErrorDto, isMine bool, postCount, commentCount int) *dtos.ProfilPageDto {
+	if err == nil {
+		err = &dtos.ProfilePageErrorDto{}
+	}
+	data := dtos.ProfilPageDto{
+		Header: dtos.HeaderDto{
+			IsConnected: isConnected,
+		},
+		Username:       user.Pseudo,
+		Email:          user.Email,
+		JoinedAt:       user.CreatedAt.Format("January 02, 2006"),
+		Avatar:         template.URL(utils.ConvertBytesToBase64(user.Avatar, "image/png")),
+		Bio:            user.Bio,
+		PostsCount:     postCount,
+		CommentsCount:  commentCount,
+		RecentActivity: RecentActivity, // This should be populated with actual recent activity data
+		Error:          *err,
+		IsMine:         isMine,
+	}
+	return &data
+}
+
+func getUserIdFromURL(
+	w http.ResponseWriter,
+	r *http.Request,
+	isConnected bool,
+	connectedUserId *uuid.UUID) uuid.UUID {
+	userId := r.URL.Query().Get("user_id")
+	if userId == "" { // If userId is not set
+		if isConnected { // And user is connected, redirect him to his profile
+			http.Redirect(w, r, "/me", http.StatusSeeOther)
+			return uuid.Nil
+		}
+		ShowError400(w, &dtos.HeaderDto{IsConnected: isConnected}) // Else, send a bad request error
+		return uuid.Nil
+	}
+
+	userUuid, err := uuid.Parse(userId)     // Parse the id from URL into a uuid
+	if err != nil || userUuid == uuid.Nil { // If the parse fail
+		ShowError400(w, &dtos.HeaderDto{IsConnected: isConnected}) // Send a bad request error due to invalid ID
+		return uuid.Nil
+	}
+	if connectedUserId != nil && userUuid == *connectedUserId { // If the user_id from URL is corresponding to the id of the connected user
+		http.Redirect(w, r, "/me", http.StatusSeeOther) // Redirect the user to his profile
+		return uuid.Nil
+	}
+
+	return userUuid
 }
