b5c766171a316fcc27bc4ab6507676e21e45701f
Mloan <loan.mata@ynov.com>
2025-06-13 00:12:20 +0200

    feat(dtos): add management flags to AdminPageDto for enhanced functionality

--- code/src/pkg/dtos/templates/adminPageDto.go
+++ code/src/pkg/dtos/templates/adminPageDto.go
@@ -3,8 +3,11 @@ package dtos
 import "Forum-back/pkg/models"
 
 type AdminPageDto struct {
-	Header        HeaderDto           `json:"header"`
-	AllUsers      []models.User       `json:"all_users"`
-	AllPost       *[]*models.Post     `json:"all_post"`
-	AllCategories *[]*models.Category `json:"all_category"`
+	Header             HeaderDto           `json:"header"`
+	AllUsers           []*models.User      `json:"all_users"`
+	AllPost            *[]*models.Post     `json:"all_post"`
+	AllCategories      *[]*models.Category `json:"all_category"`
+	UserManagement     bool                `json:"user_management"`
+	ContentManagement  bool                `json:"content_management"`
+	CategoryManagement bool                `json:"category_management"`
 }
