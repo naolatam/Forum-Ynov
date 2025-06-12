1d336301a20003452030df475d5ae3357ae5b02e
Mloan <loan.mata@ynov.com>
2025-06-13 00:11:55 +0200

    fix(adminRoutes): require authentication for admin route handlers

--- code/src/internal/routes/adminRoutes.go
+++ code/src/internal/routes/adminRoutes.go
@@ -8,6 +8,11 @@ import (
 )
 
 func initAdminRoutes() {
-	http.HandleFunc("/admin", mw.GetMethodOnly(mw.WithDB(mw.WithAuth(mw.WithHeader((handlers.AdminHandler))))))
+	http.HandleFunc("/admin", mw.GetMethodOnly(mw.WithDB(mw.WithAuthRequired(mw.WithHeader((handlers.AdminHandler))))))
+
+	http.HandleFunc("/admin/user/search", mw.GetMethodOnly(mw.WithDB(mw.WithAuthRequired(mw.WithHeader((handlers.AdminSearchUserHandler))))))
+	http.HandleFunc("/admin/user/promote", mw.PostMethodOnly(mw.WithDB(mw.WithAuthRequired(mw.WithHeader((handlers.PromoteUser))))))
+	http.HandleFunc("/admin/user/demote", mw.PostMethodOnly(mw.WithDB(mw.WithAuthRequired(mw.WithHeader((handlers.DemoteUser))))))
+
 	log.Println("[ROUTING] Admin routes initialized")
 }
