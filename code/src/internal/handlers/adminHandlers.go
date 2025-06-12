7627ffb1c99a9bad5f153729b7305241d37c23ce
Torolgo <ylan.dessenne.spam@gmail.com>
2025-06-12 21:42:33 +0200

    feat(admin): enhance AdminHandler to include user, post, and category services with improved error handling

--- code/src/internal/handlers/adminHandlers.go
+++ code/src/internal/handlers/adminHandlers.go
@@ -1,23 +1,43 @@
 package handlers
 
 import (
+	"Forum-back/internal/templates"
 	dtos "Forum-back/pkg/dtos/templates"
+	"Forum-back/pkg/models"
+	"Forum-back/pkg/services"
+	"database/sql"
 	"net/http"
-	"os"
 )
 
-func AdminHandler(w http.ResponseWriter, r *http.Request) {
-	templatePath := "internal/templates/admin.gohtml"
+func AdminHandler(w http.ResponseWriter, r *http.Request, db *sql.DB, session *models.Session, header *dtos.HeaderDto) {
 
-	if _, err := os.Stat(templatePath); os.IsNotExist(err) {
-		errorData := dtos.ErrorPageDto{
-			Code:    http.StatusNotFound,
-			Message: "Not found",
-			Details: "If you are part of the support please check the path of the : " + templatePath,
-		}
-		ShowError(w, errorData)
+	userService := services.NewUserService(db)
+	postService := services.NewPostService(db)
+	categoryService := services.NewCategoryService(db)
+
+	allUsers, _ := userService.GetAllUsers()
+	allPost, _ := postService.FindPostByQueryAndCategory("", nil)
+	allCategories := categoryService.FindAll()
+
+	data := dtos.AdminPageDto{
+		Header:        *header,
+		AllUsers:      allUsers,
+		AllPost:       allPost,
+		AllCategories: allCategories,
+	}
+
+	if data.Header.IsAdmin == false {
+		ShowError403(w, &data.Header)
 		return
 	}
 
-	http.ServeFile(w, r, templatePath)
+	tmpl, err := templates.GetTemplateWithLayout(&data.Header, "admin", "internal/templates/admin.gohtml")
+	if err != nil {
+		ShowTemplateError500(w, &data.Header)
+		return
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
