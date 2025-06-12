3677ac85afdde6de76600c5a67f18ecc703f19e0
Mloan <loan.mata@ynov.com>
2025-06-12 22:41:32 +0200

    Add notification service and update comment/post reaction handlers
    - Introduced NotificationService in comment and post reaction handlers to create notifications for new comments and reactions.
    - Enhanced notification repository to handle user and post validation, deleting notifications if the associated user or post is not found.
    - Updated SQL queries in the notification repository to include additional fields for better notification management.

--- code/src/pkg/repositories/notificationRepository.go
+++ code/src/pkg/repositories/notificationRepository.go
@@ -11,6 +11,8 @@ import (
 
 type NotificationRepository struct {
 	db *sql.DB
+	ur *UserRepository
+	pr *PostRepository
 }
 
 func (repository *NotificationRepository) FindByUserId(userId uuid.UUID) (notifs *[]*models.Notification, err error) {
@@ -19,7 +21,7 @@ func (repository *NotificationRepository) FindByUserId(userId uuid.UUID) (notifs
 	}
 
 	// Prepare the SQL statement
-	rows, err := repository.db.Query(`SELECT id, title, description, createdAt FROM notifications WHERE user_id = ?`, userId)
+	rows, err := repository.db.Query(`SELECT id, title, description, createdAt, from_user_id, post_id FROM notifications WHERE user_id = ?`, userId)
 	if err != nil {
 		return nil, err
 	}
@@ -29,10 +31,23 @@ func (repository *NotificationRepository) FindByUserId(userId uuid.UUID) (notifs
 	for rows.Next() {
 		var notif models.Notification
 		notif.User_ID = userId // Set the user ID for each activity
-		if err := rows.Scan(&notif.ID, &notif.Title, &notif.Description, &notif.CreatedAt); err != nil {
+		if err := rows.Scan(&notif.ID, &notif.Title, &notif.Description, &notif.CreatedAt, &notif.FromUser_ID, &notif.Post_ID); err != nil {
 			return nil, err
 		}
 		notif.TimeAgo = utils.TimeAgo(notif.CreatedAt) // Convert the createdAt time to a human-readable format
+
+		if user, err := repository.ur.FindById(notif.FromUser_ID); user != nil && err == nil {
+			notif.FromUser = *user // Set the FromUser field if the user is found
+		} else {
+			repository.Delete(notif.ID) // If the user is not found, delete the notification
+			continue
+		}
+		if post, err := repository.pr.FindById(notif.Post_ID); post != nil && err == nil {
+			notif.Post = *post // Set the Post field if the post is found
+		} else {
+			repository.Delete(notif.ID) // If the post is not found, delete the notification
+			continue
+		}
 		notifList = append(notifList, &notif)
 	}
 
@@ -50,15 +65,30 @@ func (repository *NotificationRepository) Create(notif *models.Notification) (su
 	}
 
 	// Prepare the SQL statement
-	_, err = repository.db.Exec(`INSERT INTO notifications(title, description, createdAt, user_id) 
-	VALUES (?,?,?,?)`,
+	_, err = repository.db.Exec(`INSERT INTO notifications(title, description, createdAt, from_user_id, user_id, post_id) 
+	VALUES (?,?,?,?,?,?)`,
 		notif.Title,
 		notif.Description,
 		notif.CreatedAt,
+		notif.FromUser_ID,
 		notif.User_ID,
+		notif.Post_ID,
 	)
 	if err != nil {
 		return false, err
 	}
 	return true, nil
+}
+
+func (repository *NotificationRepository) Delete(id int64) (success bool, err error) {
+	if repository.db == nil {
+		return false, errors.New("connection to database isn't established")
+	}
+	// Prepare the SQL statement
+	_, err = repository.db.Exec(`DELETE FROM notifications WHERE id = ?`, id)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+
 }
