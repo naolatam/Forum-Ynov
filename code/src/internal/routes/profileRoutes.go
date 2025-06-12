848af69d23c66bcf437f501a84d4d5cb23290900
Mloan <loan.mata@ynov.com>
2025-06-13 01:02:28 +0200

    feat(routes): change profile edit and delete routes to use POST method

--- code/src/internal/routes/profileRoutes.go
+++ code/src/internal/routes/profileRoutes.go
@@ -12,8 +12,8 @@ func initProfileRoute() {
 	http.HandleFunc("/profile", mw.GetMethodOnly(mw.WithDB(mw.WithAuth(mw.WithHeader(handlers.ProfileHandler)))))
 
 	http.HandleFunc("/me", mw.GetMethodOnly(mw.WithDB(mw.WithRequiredAuthRedirect("/auth/login", mw.WithHeader(handlers.MyProfileHandler)))))
-	http.HandleFunc("/me/delete", mw.GetMethodOnly(mw.WithDB(mw.WithRequiredAuthRedirect("/auth/login", mw.WithHeader(handlers.DeleteMyProfileHandler)))))
-	http.HandleFunc("/me/edit", mw.GetMethodOnly(mw.WithDB(mw.WithRequiredAuthRedirect("/auth/login", mw.WithHeader(handlers.EditMyProfileHandler)))))
+	http.HandleFunc("/me/delete", mw.PostMethodOnly(mw.WithDB(mw.WithRequiredAuthRedirect("/auth/login", mw.WithHeader(handlers.DeleteMyProfileHandler)))))
+	http.HandleFunc("/me/edit", mw.PostMethodOnly(mw.WithDB(mw.WithRequiredAuthRedirect("/auth/login", mw.WithHeader(handlers.EditMyProfileHandler)))))
 
 	log.Println("[ROUTING] Profile routes initialized")
 }
