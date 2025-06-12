ce481be36afd4cde29e80c16aea80b62d3302c3c
Mloan <loan.mata@ynov.com>
2025-06-12 15:17:31 +0200

    fix(dtos): update HeaderDto and ProfilPageDto to include Notifications and correct RecentActivity type

--- code/src/pkg/dtos/templates/profilePageDto.go
+++ code/src/pkg/dtos/templates/profilePageDto.go
@@ -1,18 +1,21 @@
 package dtos
 
-import "html/template"
+import (
+	"Forum-back/pkg/models"
+	"html/template"
+)
 
 type ProfilPageDto struct {
-	Header         HeaderDto            `json:"header"`
-	Username       string               `json:"username"`
-	Email          string               `json:"email"`
-	JoinedAt       string               `json:"joined_at"`
-	Avatar         template.URL         `json:"avatar"`
-	Bio            string               `json:"bio"`
-	PostsCount     int                  `json:"posts_count"`
-	CommentsCount  int                  `json:"comments_count"`
-	RecentActivity []*RecentActivityDto `json:"recent_activity"`
-	Error          ProfilePageErrorDto  `json:"error_string,omitempty"`
+	Header         HeaderDto                `json:"header"`
+	Username       string                   `json:"username"`
+	Email          string                   `json:"email"`
+	JoinedAt       string                   `json:"joined_at"`
+	Avatar         template.URL             `json:"avatar"`
+	Bio            string                   `json:"bio"`
+	PostsCount     int                      `json:"posts_count"`
+	CommentsCount  int                      `json:"comments_count"`
+	RecentActivity []*models.RecentActivity `json:"recent_activity"`
+	Error          ProfilePageErrorDto      `json:"error_string,omitempty"`
 	IsMine         bool
 }
 
