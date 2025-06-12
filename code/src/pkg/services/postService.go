69d21dee6db5f5e8b27dd3cdceb70448a4df11ee
Mloan <loan.mata@ynov.com>
2025-06-12 21:12:01 +0200

    feat(post): add methods for updating and deleting posts, and managing categories

--- code/src/pkg/services/postService.go
+++ code/src/pkg/services/postService.go
@@ -1,12 +1,15 @@
 package services
 
 import (
+	dtos "Forum-back/pkg/dtos/templates"
 	"Forum-back/pkg/models"
 	"Forum-back/pkg/repositories"
 	"Forum-back/pkg/utils"
 	"fmt"
 	"html/template"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -14,6 +17,7 @@ import (
 type PostService struct {
 	repo *repositories.PostRepository
 	ur   *repositories.UserRepository
+	cr   *repositories.CategoryRepository
 }
 
 func (s *PostService) FindPostByQueryAndCategory(searchTerm string, categoryID *uuid.UUID) (*[]*models.Post, error) {
@@ -100,4 +104,109 @@ func (service *PostService) GetUserPostCount(user *models.User) int {
 	count, _ := service.repo.GetUserPostCount(&user.ID)
 
 	return count
+}
+
+func (service *PostService) UpdateCategoryFromList(categories []string, post *models.Post) dtos.ErrorPageDto {
+	for _, categoryID := range categories {
+		if categoryID == "" {
+			return dtos.ErrorPageDto{
+				Header:  dtos.HeaderDto{},
+				Message: "Category ID cannot be empty.",
+				Details: "Please provide a valid category ID.",
+				Code:    http.StatusBadRequest,
+			}
+		}
+		validCategoryId, err := uuid.Parse(categoryID)
+		if err != nil {
+			return dtos.ErrorPageDto{
+				Header:  dtos.HeaderDto{},
+				Message: "Invalid category ID format.",
+				Details: fmt.Sprintf("The provided category ID '%s' is not a valid UUID.", categoryID),
+				Code:    http.StatusBadRequest,
+			}
+		}
+		c, err := service.cr.FindById(&validCategoryId)
+		if c != nil && err == nil {
+			post.Categories = append(post.Categories, c)
+		}
+	}
+
+	if err := service.UpdateCategory(post); err != nil {
+		return dtos.ErrorPageDto{
+			Header:  dtos.HeaderDto{},
+			Message: "Error while updating post category",
+			Details: fmt.Sprintf("Error while updating post category: %s", err.Error()),
+			Code:    http.StatusInternalServerError,
+		}
+	}
+	return dtos.ErrorPageDto{
+		Code: http.StatusOK,
+	}
+}
+
+func (service *PostService) UpdateCategory(post *models.Post) error {
+	if post == nil || post.ID == 0 {
+		return fmt.Errorf("post is nil or has no ID")
+	}
+
+	if err := service.cr.DeleteCategoryByPostId(post.ID); err != nil {
+		return fmt.Errorf("error updating existing categories for post ID %d: %s", post.ID, err)
+	}
+
+	for _, category := range post.Categories {
+		if category == nil || category.ID == uuid.Nil {
+			continue
+		}
+		if err := service.cr.AssociateCategoryToAPost(category.ID, post.ID); err != nil {
+			return fmt.Errorf("error adding category %s to post ID %d: %s", category.Name, post.ID, err)
+		}
+	}
+
+	return nil
+}
+
+func (service *PostService) UpdatePost(post *models.Post) error {
+	if post == nil || post.ID == 0 {
+		return fmt.Errorf("post is nil or has no ID")
+	}
+
+	if err := service.repo.UpdatePost(post); err != nil {
+		return fmt.Errorf("error updating post ID %d: %s", post.ID, err)
+	}
+
+	return nil
+}
+
+func (service *PostService) Delete(post *models.Post) error {
+	if post == nil || post.ID == 0 {
+		return fmt.Errorf("post is nil or has no ID")
+	}
+
+	if err := service.repo.Delete(post); err != nil {
+		return fmt.Errorf("error deleting post ID %d: %s", post.ID, err)
+	}
+	return nil
+}
+
+func (service *PostService) Create(post *models.Post) error {
+	if post == nil {
+		return fmt.Errorf("post is nil")
+	}
+
+	post.CreatedAt = time.Now()
+	if post.Picture == nil {
+		post.Picture = utils.GetDefaultAvatar()
+	}
+
+	if err := service.repo.Create(post); err != nil {
+		return fmt.Errorf("error creating post: %s", err)
+	}
+
+	if len(post.Categories) > 0 {
+		if err := service.UpdateCategory(post); err != nil {
+			return fmt.Errorf("error associating categories with post: %s", err)
+		}
+	}
+
+	return nil
 }
